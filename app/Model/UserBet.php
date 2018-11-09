1dd3ee014bb0c38ec61dc54eb75cd0a111370dc3
Sirius <358008599@qq.com>
2018-11-09 16:23:38 +0800

    1

--- app/Model/UserBet.php
+++ app/Model/UserBet.php
@@ -6,9 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserBet extends Model
 {
-    use \App\Traits\HasCompositePrimaryKey;
+    //use \App\Traits\HasCompositePrimaryKey;
 
-    protected $primaryKey = ['user_id', 'game_id'];         // 设置组合主键
+    protected $primaryKey = 'user_id';         // 设置组合主键
 
     public $keyType = 'string';
     public $incrementing = false;
